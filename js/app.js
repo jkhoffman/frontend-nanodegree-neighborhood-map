bd78be1c8e4e2404ee6ec5cf647b3b84d5ea789b
Jeff Hoffman <jkhoffman@gmail.com>
2015-06-28 02:11:51 -0400

    Add search box with real-time filtering.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,37 @@
-var NeighborhoodMapViewModel = function(mapOptions) {
-    this.map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+var NeighborhoodMapViewModel = function(locations) {
+    var self = this;
+
+    var mapOptions = {
+        center: { lat: 35.1271595, lng: -80.7088114 },
+        zoom: 15
+    };
+
+    self.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    self.locations = ko.observableArray(locations);
+
+	self.query = ko.observable('');
+
+	self.visibleLocations = ko.computed(
+		function() {
+			return self.locations().filter(
+				function(location) {
+					return self.query() === '' ? true : location.name.startsWith(self.query());
+				}
+			);
+		}, this);
 };
 
-ko.applyBindings(new NeighborhoodMapViewModel({
-    center: { lat: 35.1271595, lng: -80.7088114},
-    zoom: 14
-}));
+ko.applyBindings(
+    new NeighborhoodMapViewModel([
+        {
+            name: 'Matthews Elementary School'
+        },
+        {
+            name: 'BBQ'
+        },
+        {
+            name: 'Ice Cream'
+        }
+    ])
+);
